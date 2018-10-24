bf9e9dcbc87f7cc5442cee8a0356f27ab9a44733
Alexia E. Smith <washuu@gmail.com>
2018-10-24 12:10:15 -0700

    Update from Hydra.

--- CacheBreaker.hooks.php
+++ CacheBreaker.hooks.php
@@ -11,20 +11,20 @@
  *
  **/
 
-class cacheBreakerHooks {
+class CacheBreakerHooks {
 	/**
 	 * Handles modifying thumbnail URL links.
 	 *
 	 * @access	public
 	 * @param	object	ThumbnailImage Object
-	 * @param	array	Attributes to be set on the IMG 
+	 * @param	array	Attributes to be set on the IMG
 	 * @param	array	Attributes to be set on the A HREF tag.
 	 * @return	boolean	true
 	 */
 	static public function onThumbnailBeforeProduceHTML(ThumbnailImage $thumbnailImage, &$attributes, &$linkAttributes) {
 		global $wgUploadPath;
 
-		$cacheHash = 'version='.md5($thumbnailImage->getFile()->getRepo()->getFileTimestamp($thumbnailImage->getStoragePath()).$thumbnailImage->getWidth().$thumbnailImage->getHeight().$thumbnailImage->getUrl());
+		$cacheHash = 'version='.md5($thumbnailImage->getFile()->getRepo()->getFileTimestamp($thumbnailImage->getFile()->getPath()).$thumbnailImage->getWidth().$thumbnailImage->getHeight().$thumbnailImage->getUrl());
 
 		if (!empty($attributes['src'])) {
 			$attributes['src'] = self::appendHash($attributes['src'], $cacheHash);
@@ -45,6 +45,29 @@ class cacheBreakerHooks {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Append the cache hash to [[Media:...]] links.
+	 *
+	 * @param	object	Title
+	 * @param	object	File
+	 * @param	string	HTML inside the anchor tag.
+	 * @param	array	Array of tag attributes.
+	 * @param	string	The raw return: "Html::rawElement( 'a', $attribs, $html );"
+	 * @return	boolean	False - Returning false is required to trigger the MediaWiki to know that &$return was modified.
+	 */
+	static public function onLinkerMakeMediaLinkFile($title, $file, &$html, &$attributes, &$return) {
+		if (empty($attributes['href']) || !$file) {
+			return true;
+		}
+
+		$cacheHash = 'version='.md5($file->getRepo()->getFileTimestamp($file->getPath()).$file->getWidth().$file->getHeight().$file->getUrl());
+
+		$attributes['href'] = self::appendHash($attributes['href'], $cacheHash);
+
+		$return = Html::rawElement('a', $attributes, $html);
+		return false;
 	}
 
 	/**
@@ -85,35 +108,73 @@ class cacheBreakerHooks {
 	 * Handles adding cache breaker to $wgLogo.
 	 *
 	 * @access	public
-	 * @param	object	SkinTemplate Object
-	 * @param	object	Initialized SkinTemplate Object
-	 * @return	boolean True
+	 * @param	object	[Unused] Title
+	 * @param	object	[Unused] Article
+	 * @param	object	[Unused] Output
+	 * @param	object	[Unused] User
+	 * @param	object	[Unused] WebRequest
+	 * @param	object	[Unused] Mediawiki
+	 * @return	boolean	True
 	 */
-	static public function onSkinTemplateOutputPageBeforeExec(&$te, &$tpl) {
-		if (!isset($tpl->data)) {
-			return true;
-		}
-		if (isset($tpl->data['logopath'])) {
-			$pathInfo = explode('/', $tpl->data['logopath']);
-			$file = array_pop($pathInfo);
-			$fileMD5 = md5($file);
-			$md5_2 = array_pop($pathInfo);
-			$md5_1 = array_pop($pathInfo);
+	static public function onBeforeInitialize(&$title = null, &$article = null, &$output = null, &$user = null, $request = null, $mediaWiki = null) {
+		global $wgLogo, $wgLogoHD, $wgFavicon;
 
-			if (substr($fileMD5, 0, 1) == $md5_1 && substr($fileMD5, 0, 2) == $md5_2) {
-				$_file = trim($file);
-				$_file = wfLocalFile(Title::newFromText('File:'.$_file));
-
-				if ($_file !== null && $_file->exists()) {
-					$cacheHash = 'version='.md5($_file->getTimestamp().$_file->getWidth().$_file->getHeight());
-
-					$tpl->set(
-						'logopath',
-						self::appendHash($tpl->data['logopath'], $cacheHash)
-					);
-				}
+		$wgLogo = self::appendHashToUrl($wgLogo);
+		if (is_array($wgLogoHD)) {
+			foreach ($wgLogoHD as $key => $url) {
+				$wgLogoHD[$key] = self::appendHashToUrl($url);
 			}
 		}
+		$wgFavicon = self::appendHashToUrl($wgFavicon);
+
+		return true;
+	}
+
+	/**
+	 * Append the cache hash to the URL based on the file name.
+	 *
+	 * @access	private
+	 * @param	string	Original URL
+	 * @return	string	Modified URL or the original URL on error.
+	 */
+	static private function appendHashToUrl($url) {
+		$pathInfo = explode('/', $url);
+		$file = array_pop($pathInfo);
+		$fileMD5 = md5($file);
+		$md5_2 = array_pop($pathInfo);
+		$md5_1 = array_pop($pathInfo);
+
+		if (substr($fileMD5, 0, 1) == $md5_1 && substr($fileMD5, 0, 2) == $md5_2) {
+			$_file = trim($file);
+			$_file = wfLocalFile(Title::newFromText('File:'.$_file));
+
+			if ($_file !== null && $_file->exists()) {
+				$cacheHash = 'version='.md5($_file->getTimestamp().$_file->getWidth().$_file->getHeight());
+
+				return self::appendHash($url, $cacheHash);
+			}
+		}
+		return $url;
+	}
+
+	/**
+	 * Handles adding cache breaker to $wgLogo.
+	 *
+	 * @access	public
+	 * @param	object	Title Object
+	 * @param	array	URLs to be purged.
+	 * @return	boolean True
+	 */
+	static public function onTitleSquidURLs(Title $title, array &$urls) {
+		foreach ($urls as $url) {
+			if (strpos($url, 'https://') === 0) {
+				$urls[] = str_replace('https://', 'http://', $url);
+			}
+			if (strpos($url, 'http://') === 0) {
+				$urls[] = str_replace('http://', 'https://', $url);
+			}
+		}
+		$urls = array_unique($urls);
 
 		return true;
 	}
